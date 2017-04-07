0343d2099e7b9b3f410c41db4287cf9d4c002048
ValoraSun <Valora.Sun@gmail.com>
2017-04-07 17:04:07 +0800

    JWT改名

--- src/main/java/com/sc/utils/JWT.java
+++ src/main/java/com/sc/utils/JWT.java
@@ -1,4 +1,4 @@
-package com.sc.jwt;
+package com.sc.utils;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtBuilder;
@@ -9,12 +9,13 @@ import io.jsonwebtoken.SignatureAlgorithm;
  * 生成Token。
  * Created by valora on 2017/4/5.
  */
-public class JwtHelper {
+public class JWT {
     //解析Jwt
     public static Claims parseJWT(String jsonWebToken) {
         try {
             Claims claims = Jwts.parser()
-//                    .setSigningKey(DatatypeConverter.parseBase64Binary(base64Security))
+                    //签名
+                    .setSigningKey("")
                     .parseClaimsJws(jsonWebToken).getBody();
             return claims;
         } catch (Exception ex) {
@@ -22,17 +23,15 @@ public class JwtHelper {
         }
     }
     
+    //生成token
     public static String createJWT(String account){
         SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
 
-//        byte[] apiLeySecretBytes = DatatypeConverter.parseBase64Binary(base64Security);
-//        Key signingKey = new SecretKeySpec(apiLeySecretBytes, signatureAlgorithm.getJcaName());
-
         JwtBuilder builder = Jwts.builder()
                 .setHeaderParam("type", "JWT")  //头信息
-                .claim("account", account);  //数据声明信息
+                .claim("account", account)  //数据声明信息
                 //.setExpiration()  //过期时间设置。
-//                .signWith(signatureAlgorithm, signingKey); //签名
+                .signWith(signatureAlgorithm,""); //签名
         
         return builder.compact();
     }
