8a41e2b4f7895f3717c99a44ca9cb488f0f2fd7b
ValoraSun <Valora.Sun@gmail.com>
2017-05-12 17:28:50 +0800

    。。

--- src/main/java/com/sc/web/UserDoMainController.java
+++ src/main/java/com/sc/web/UserDoMainController.java
@@ -1,5 +1,9 @@
 package com.sc.web;
 
+import com.sc.utils.Result;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -8,4 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class UserDoMainController {
+    private final static String URL = "/api/UserDoMain/";
+
+    @RequestMapping(value = URL + "SubmitOrder", method = RequestMethod.POST)
+    @ApiOperation("提交订单{传入参数-->秘钥:token，商品信息:goodsjson}")
+    public Result submitOrder() {
+        return null;
+    }
 }
