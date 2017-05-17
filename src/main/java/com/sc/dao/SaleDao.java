ddbdb108ca2e2af4504fbae73b3c5384c702ed3a
mzd123 <707307313@qq.com>
2017-05-17 16:07:43 +0800

    aa

--- src/main/java/com/sc/dao/SaleDao.java
+++ src/main/java/com/sc/dao/SaleDao.java
@@ -1,6 +1,22 @@
 package com.sc.dao;
 
+import com.sc.domain.generator.Admins;
+import com.sc.domain.generator.AdminsExample;
+import com.sc.domain.generator.Register;
+import com.sc.domain.generator.RegisterExample;
+import com.sc.domain.generator.Sellers;
+import com.sc.domain.generator.SellersExample;
+import com.sc.domain.generator.Users;
+import com.sc.domain.generator.UsersExample;
+import com.sc.mapper.generator.AdminsMapper;
+import com.sc.mapper.generator.RegisterMapper;
+import com.sc.mapper.generator.SellersMapper;
+import com.sc.mapper.generator.UsersMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * SaleDao
@@ -8,4 +24,141 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class SaleDao {
+    private final RegisterMapper registerMapper;
+
+    private final UsersMapper usersMapper;
+    private final SellersMapper sellersMapper;
+    private final AdminsMapper adminsMapper;
+
+    @Autowired
+    public SaleDao(RegisterMapper registerMapper, UsersMapper usersMapper, SellersMapper sellersMapper, AdminsMapper adminsMapper) {
+        this.registerMapper = registerMapper;
+        this.usersMapper = usersMapper;
+        this.sellersMapper = sellersMapper;
+        this.adminsMapper = adminsMapper;
+    }
+
+    /**
+     * 根据电话号码查询用户
+     *
+     * @param phone 查询条件
+     * @return 查询结果集
+     */
+    public List<Register> selectregisterbyphone(Long phone) {
+        RegisterExample registerExample = new RegisterExample();
+        RegisterExample.Criteria criteria = registerExample.createCriteria();
+        criteria.andCmPhoneEqualTo(phone);
+        return registerMapper.selectByExample(registerExample);
+    }
+
+    /**
+     * 新用户时，对表进行insert操作
+     *
+     * @param register register对象
+     */
+    public void addregister(Register register) {
+        registerMapper.insert(register);
+    }
+
+    /**
+     * 不是新用户时，对表进行更新操作
+     *
+     * @param register register对象
+     * @param phone    手机号码
+     */
+    public void updateregister(Register register, Long phone) {
+        RegisterExample registerExample = new RegisterExample();
+        RegisterExample.Criteria criteria = registerExample.createCriteria();
+        criteria.andCmPhoneEqualTo(phone);
+        registerMapper.updateByExample(register, registerExample);
+    }
+
+    /**
+     * 根据经纬度获取商家列表
+     *
+     * @param lon1 最小经度
+     * @param lon2 最大经度
+     * @param lat1 最小纬度
+     * @param lat2 最大纬度
+     * @return 商家列表
+     */
+    public List<Users> selectuserByMap(double lon1, double lon2, double lat1, double lat2) {
+        UsersExample usersExample = new UsersExample();
+        UsersExample.Criteria criteria = usersExample.createCriteria();
+        criteria.andCmShoplonGreaterThan(lon1);
+        criteria.andCmShoplonLessThan(lon2);
+        criteria.andCmShoplatGreaterThan(lat1);
+        criteria.andCmShoplatLessThan(lat2);
+        List<Users> list = usersMapper.selectByExample(usersExample);
+        return list;
+    }
+
+    /**
+     * 根据经纬度获取厂家列表
+     *
+     * @param lon1 最小经度
+     * @param lon2 最大经度
+     * @param lat1 最小纬度
+     * @param lat2 最大纬度
+     * @return 厂家列表
+     */
+    public List<Sellers> selectSellersByMap(double lon1, double lon2, double lat1, double lat2) {
+        SellersExample sellersExample = new SellersExample();
+        SellersExample.Criteria criteria = sellersExample.createCriteria();
+        criteria.andCmLonGreaterThan(lon1);
+        criteria.andCmLonLessThan(lon2);
+        criteria.andCmLonGreaterThan(lat1);
+        criteria.andCmLatLessThan(lat2);
+        List<Sellers> list = sellersMapper.selectByExample(sellersExample);
+        return list;
+    }
+
+    /**
+     * 根据手机号码查询admin
+     *
+     * @param phone 手机号码
+     * @return admin对象
+     */
+    public Admins selectadminbyphone(Long phone) {
+        AdminsExample adminsExample = new AdminsExample();
+        AdminsExample.Criteria criteria = adminsExample.createCriteria();
+        criteria.andCmPhoneEqualTo(phone);
+        List<Admins> list = adminsMapper.selectByExample(adminsExample);
+        Admins admins = list.get(0);
+        return admins;
+
+    }
+
+    /**
+     * 更新业务人员的密码
+     *
+     * @param admins 业务人员对象
+     */
+    public void updateAdminPassword(Admins admins) {
+        AdminsExample adminsExample = new AdminsExample();
+        AdminsExample.Criteria criteria = adminsExample.createCriteria();
+        criteria.andCmAdminidEqualTo(admins.getCmAdminid());
+        adminsMapper.updateByExample(admins, adminsExample);
+    }
+
+    /**
+     * 查询手机号为phone，Lelve不为1的业务人员
+     *
+     * @param phone 手机号
+     * @param i     Lelve
+     * @return 业务人员
+     */
+    public Admins selectadminbyphone_and_Level(Long phone, int i) {
+        AdminsExample adminsExample = new AdminsExample();
+        AdminsExample.Criteria criteria = adminsExample.createCriteria();
+        criteria.andCmPhoneEqualTo(phone);
+        List<Integer> list = new ArrayList<Integer>();
+        list.add(i);
+        criteria.andCmLevelNotIn(list);
+        List<Admins> list2 = adminsMapper.selectByExample(adminsExample);
+        if (list2 != null && list2.size() > 0) {
+            return list2.get(0);
+        }
+        return null;
+    }
 }
