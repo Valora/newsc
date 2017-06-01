bfe9c02c87ebb8060f8929013beb8683084e6d63
ValoraSun <Valora.Sun@gmail.com>
2017-06-01 14:54:43 +0800

    paycontroller

--- src/main/java/com/sc/web/PayStartCallController.java
+++ src/main/java/com/sc/web/PayStartCallController.java
@@ -1,9 +1,11 @@
 package com.sc.web;
 
+import com.sc.service.PayService;
 import com.sc.utils.Result;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,24 +20,27 @@ import org.springframework.web.bind.annotation.RestController;
 public class PayStartCallController {
     private static final String URL = "/api/PayStartCall/";
 
+    @Autowired
+    private PayService payService;
+
     @RequestMapping(value = "WechatPay_app", method = RequestMethod.GET)
     @ApiOperation("微信（APP）")
     @ApiImplicitParam(name = "orderid", value = "订单ID", required = true, dataType = "String", paramType = "query")
-    public Result wechatPayApp(@RequestParam("orderid") String orderid) {
-        return null;
+    public Result wechatPayApp(@RequestParam("orderids") String orderids) {
+        return payService.wechatPayApp(orderids);
     }
 
     @RequestMapping(value = "WechatPay_pc", method = RequestMethod.GET)
     @ApiOperation("微信（PC）")
     @ApiImplicitParam(name = "orderid", value = "订单ID", required = true, dataType = "String", paramType = "query")
-    public Result wechatPayPc(@RequestParam("orderid") String orderid) {
-        return null;
+    public Result wechatPayPc(@RequestParam("orderids") String orderids) {
+        return payService.wechatPayPC(orderids);
     }
 
     @RequestMapping(value = "AliPay_app", method = RequestMethod.GET)
     @ApiOperation("支付宝（APP）")
     @ApiImplicitParam(name = "orderid", value = "订单ID", required = true, dataType = "String", paramType = "query")
-    public Result aliPayApp(@RequestParam("orderid") String orderid) {
-        return null;
+    public Result aliPayApp(@RequestParam("orderids") String orderids) {
+        return payService.aliPayApp(orderids);
     }
 }
